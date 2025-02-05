3a6c95d9a6cf7f5dead804631c59ca3f75a43b43
David <drajnoha@seznam.cz>
2025-02-05 11:08:57 -0500

    feat: explicitely checking division by zero raising ZeroDivisionError

--- calculator/operations.py
+++ calculator/operations.py
@@ -10,4 +10,7 @@ def multiply(a: float, b: float) -> float:
     return a * b
 
 def divide(a: float, b: float) -> float:
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+
     return a / b
